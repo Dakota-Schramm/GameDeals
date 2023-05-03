1243322fe78d82c9cc637878bfec68ad792dcab0
Dakota Schramm <william.schramm11@gmail.com>
2023-05-03 18:12:02 -0500

    Moved container to layout.

--- app/catalogue/layout.tsx
+++ app/catalogue/layout.tsx
@@ -4,7 +4,9 @@ const CatalogueLayout = ({children}) => {
   return (
     <main className='w-[360px] h-[800px] lg:w-[1024px] lg:h-[1080px] bg-gray-100 dark:bg-slate-700 mx-auto my-10'>
       <section className='flex items-center justify-center w-full h-full p-8 border border-red-600 border-solid lg:p-16'>
-        {children}
+        <section className='flex flex-col justify-between'>
+          {children}
+        </section>
       </section>
     </main>
   )
