ea4c637568c5f9d2b9fc06b2dafc2f4b1ed9b261
Dakota Schramm <william.schramm11@gmail.com>
2023-05-05 18:16:11 -0500

    Added catalogue layout.

--- app/catalogue/layout.tsx
+++ app/catalogue/layout.tsx
@@ -4,7 +4,7 @@ const CatalogueLayout = ({children}) => {
   return (
     <main className='w-[360px] h-[800px] lg:w-[1024px] lg:h-[1080px] bg-gray-100 dark:bg-slate-700 mx-auto my-10'>
       <section className='flex items-center justify-center w-full h-full p-8 border border-red-600 border-solid lg:p-16'>
-        <section className='flex flex-col justify-between'>
+        <section className='flex flex-col justify-between w-full h-full'>
           {children}
         </section>
       </section>
