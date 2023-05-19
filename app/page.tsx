734fc8534e42dc3fcea43386e0c381709731ba0d
Dakota Schramm <william.schramm11@gmail.com>
2023-05-18 20:04:55 -0500

    Renamed parallel routes to routes.

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,14 @@
+'use client'
 
+import React, { useEffect } from 'react'
+import { useRouter } from 'next/router';
+
+const RootPage = ({ children }) => {
+  const router = useRouter();
+
+  useEffect(() => { router.push('/search') }, [])
+
+  return <>{ children }</>
+}
+
+export default RootPage; 
