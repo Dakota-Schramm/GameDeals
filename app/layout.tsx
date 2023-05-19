2983cfe6c37e3ef2f31fe005ffcf1bf72ec1e8c3
Dakota Schramm <william.schramm11@gmail.com>
2023-05-18 19:39:24 -0500

    Created first attempt at state with route.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,36 @@
-import React from 'react'
+import React, { useContext } from 'react'
 
 import './globals.css';
 
-/* Meta tags added by default:
-  <meta charSet="utf-8" />
-  <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-*/
+/**
+ * 
+ * @remarks Meta tags added by default:
+ * charSet="utf-8"
+ * name="viewport" content="initial-scale=1.0, width=device-width"
+ */
 
-export default function RootLayout({ children }: {
+export default function RootLayout({catalogue, searchSettings, children}: {
+  catalogue: React.ReactNode,
+  searchSettings: React.ReactNode,
   children: React.ReactNode;
 }) {
+  const isSearch = displaySearchSection();
+
   return (
     <html className='box-border p-0 m-0'>
       <body className='box-border p-0 m-0 bg-gray-200 dark:bg-slate-900'>
-        {children}
+        { children }
+        <section>
+          { isSearch && searchSettings }
+          { !isSearch && catalogue }
+        </section>
       </body>
     </html>
   );
+}
+
+function displaySearchSection() {
+  const route = window.location.pathname;
+
+  return route.includes('?search') 
 }
