08973f2c1f3340bc08e5f57218377e824b88f5bb
Dakota Schramm <william.schramm11@gmail.com>
2023-06-03 12:15:31 -0500

    Extract imageURL hook from display.
      - Extract header and footer from DisplayGame

--- src/app/(store)/catalogue/display.tsx
+++ src/app/(store)/catalogue/display.tsx
@@ -3,33 +3,33 @@
 import React, { useEffect, useState } from 'react'
 import Link from 'next/link'
 
-import type { ICatalogueGame } from './page'
+import { ICatalogueGame } from './page'
+import { useImageURL } from '~/hooks/useImageUrl';
+
+
+const Header = ({ title, imageURL }) => (
+  <header className='flex flex-col justify-center w-full p-2 bg-red-200'>
+    <h3 className='text-center'>{title}</h3>
+    <img src={imageURL}></img>
+  </header>
+)
+
+const Footer = ({ normalPrice, salePrice }) => (
+  <footer className='flex flex-col justify-between w-full lg:flex-row'>
+    <h4 className='w-1/2 text-center bg-yellow-400'>{normalPrice}</h4>
+    <h4 className='w-1/2 text-center bg-green-400'>{salePrice}</h4>
+  </footer>
+)
 
 const DisplayGame = ({ gameID, title, normalPrice, salePrice, thumb, ...props }: ICatalogueGame) => {
-  const [image, setImage] = useState(undefined);
-
-  // ! This is not correct
-  // useEffect(() => {
-  //   fetch(thumb)
-  //   .then((res) => {
-  //     if (!res.ok) throw new Error('An error occurred while fetching the data.');
-  //     return res.blob();
-  //   })
-  //   .then((imageBlob) => setImage(imageBlob))
-  //   .catch((err) => console.log(err))}
-  // }, [])
+  const imageURL = useImageURL(thumb)
+  console.log(imageURL)
 
   return (
     <Link href={"/game/" + gameID}>
       <section className='flex flex-col items-center justify-between w-full h-full border border-blue-600 border-solid'>
-        <header className='bg-red-200'>
-          <h3>{title}</h3>
-          {/* {image} */}
-        </header>
-        <footer className='flex flex-col justify-between lg:flex-row'>
-          <h4 className='bg-yellow-400'>{normalPrice}</h4>
-          <h4 className='bg-green-400'>{salePrice}</h4>
-        </footer>
+        <Header {...{title, imageURL}}/>
+        <Footer {...{normalPrice, salePrice}} />
       </section>
     </Link>
   )
